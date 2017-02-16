76f2c97269633e4770401e8f7c13ad9d956b79f8
Lee Dong ho <dongho.lee@ge.com>
2017-02-16 10:02:53 +0900

    send chatdata to server

--- app/src/main/java/gea/com/patternchat/view/MainActivity.java
+++ app/src/main/java/gea/com/patternchat/view/MainActivity.java
@@ -16,41 +16,20 @@ import gea.com.patternchat.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-
-//    ArrayAdapter adapter = null;
-
     MainViewModel mainViewModel = new MainViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        // Bind view using DataBinding library that Android supports.
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // Set viewModel using DataBinding library.
         activityMainBinding.setViewModel(mainViewModel);
 
         mainViewModel.onCreate();
     }
-
-
-//        final String userName = "USER" + new Random().nextInt(10000);  // 랜덤한 유저 이름 설정 ex) user1234
-
-
-//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
-//        listView.setAdapter(adapter);
-
-//        sendButton.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//
-//                String currentTime = getCurrentTime();
-//
-//                    ChatData chatData = new ChatData(userName, currentTime, editText.getText().toString());
-//                    databaseReference.child("message").push().setValue(chatData);
-//                    editText.setText("");
-//            }
-//        });
 
     @Override
     protected void onPause() {
@@ -73,40 +52,4 @@ public class MainActivity extends AppCompatActivity {
         mainViewModel.onDestroy();
     }
 
-    /**
-     * Reflect changed value to listView
-     *
-     * @param chatData Chat data loaded from FireBase.
-     */
-    private void reflectToListViewAdapter(ChatData chatData) {
-
-//        adapter.add(chatData.getUserName() + "( " + chatData.getCreationTime() + " )" + ": " + chatData
-//                .getMessage());  //
-    }
-
-    /**
-     * Get dataBundle from fireBase
-     *
-     * @param dataSnapshot
-     * @return
-     */
-    ChatData getChatFromServer(DataSnapshot dataSnapshot) {
-
-        ChatData chatData = dataSnapshot.getValue(ChatData.class);  // chatData를 가져오고
-        return chatData;
-    }
-
-    /**
-     * Get current Time base on device's TIMEZONE.
-     *
-     * @return typed simpleDataFormat (yyyy:MM:dd-hh:mm:ss)
-     */
-    String getCurrentTime() {
-
-        long currentTimeMillis = System.currentTimeMillis();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd-hh:mm:ss");
-
-        String currentTime = simpleDateFormat.format(new Date(currentTimeMillis));
-        return currentTime;
-    }
 }
--- app/src/main/java/gea/com/patternchat/viewmodel/MainViewModel.java
+++ app/src/main/java/gea/com/patternchat/viewmodel/MainViewModel.java
@@ -8,14 +8,20 @@
 package gea.com.patternchat.viewmodel;
 
 import android.databinding.ObservableField;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import gea.com.patternchat.model.ChatData;
 
@@ -25,6 +31,7 @@ public class MainViewModel implements ViewModel {
     private DatabaseReference databaseReference;
 
 
+    private ChatData chatData;
     public ObservableField<String> chatDataObservableField = new ObservableField<>();
 
     public MainViewModel() {
@@ -36,6 +43,9 @@ public class MainViewModel implements ViewModel {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
+
+        chatData = new ChatData();
+        chatData.setUserName("USER");
     }
 
     @Override
@@ -54,7 +64,8 @@ public class MainViewModel implements ViewModel {
                 ChatData chatData = dataSnapshot.getValue(ChatData.class);
                 Log.d("MainViewModel", chatData.getUserName() +" : " + chatData.getMessage() );
 
-                chatDataObservableField.set(chatData.getMessage());
+                chatDataObservableField.set(chatData.getUserName() + "( " + chatData.getCreationTime() + " )" + ": " + chatData
+                        .getMessage());
 
             }
 
@@ -92,7 +103,31 @@ public class MainViewModel implements ViewModel {
 
     public void onSendButtonClicked(View view){
 
-        Log.d("onSendButtonClicked","CLICKED");
+        Log.d("onSendButtonClicked : ","SENDING CHAT TO FIREBASE SERVER");
+        databaseReference.child("message").push().setValue(chatData);
 
+    }
+
+
+    public void onTextChanged(CharSequence charSequence, int start, int before, int count){
+
+        Log.d("onTextChanged", charSequence.toString());
+
+        chatData.setCreationTime(getCurrentTime());
+        chatData.setMessage(charSequence.toString());
+    }
+
+    /**
+     * Get current Time base on device's TIMEZONE.
+     *
+     * @return typed simpleDataFormat (yyyy:MM:dd-hh:mm:ss)
+     */
+    String getCurrentTime() {
+
+        long currentTimeMillis = System.currentTimeMillis();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd-hh:mm:ss");
+
+        String currentTime = simpleDateFormat.format(new Date(currentTimeMillis));
+        return currentTime;
     }
 }
